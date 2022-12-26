8d59b5bd15f87a1c6970d57ec1c81da2bb2e161c
ropowisc <support@kwuish.com>
2022-12-26 12:37:20 -0600

    Initial Commit

---
+++ chur/churApp.swift
@@ -0,0 +1,17 @@
+//
+//  churApp.swift
+//  chur
+//
+//  Created by Rohit on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct churApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
